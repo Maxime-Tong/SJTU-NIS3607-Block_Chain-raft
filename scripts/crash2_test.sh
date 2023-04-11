fbe76f7244e6655c9d648fb224ee905943d6a7da
Lajizz <ChinW66@sjtu.edu.cn>
2023-04-11 11:14:43 +0800

    update test scripts

---
+++ scripts/crash2_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+tmux new -d -s node0 "../cmd/node -i 0 -t 20"
+tmux new -d -s node1 "../cmd/node -i 1 -t 20"
+tmux new -d -s node2 "../cmd/node -i 2 -t 20"
+tmux new -d -s node3 "../cmd/node -i 3 -t 20"
+tmux new -d -s node4 "../cmd/node -i 4 -t 20"
+tmux new -d -s node5 "../cmd/node -i 5 -t 20"
+tmux new -d -s node6 "../cmd/node -i 6 -t 20"
+
+sleep 10
+
+tmux kill-window -t node$1
+tmux kill-window -t node$2
+
+sleep 15
+
+python3 check.py 7
